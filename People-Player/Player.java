02d32235730e41896db1b4a95b79f1148d226a85
NeilMatthewLua <NeilMatthewLua>
2019-06-26 08:39:55 +0800

    Version 1.04: inJail (Get Set), Fixed attribute names and method calls

--- People-Player/Player.java
+++ People-Player/Player.java
@@ -1,35 +1,35 @@
 /**
  * Player Class
  *    
- *      New Changes:        develop()
-                            eligibleDev()
- * 
-        Last Changes Made:  Other half Roll (Trade, Purchase)    
-                            setPosition()
-                            getLastRoll()  
-                            purchase()   
-                            trade()     
+ *      New Changes:        private inJail
+                            getJail()
+                            setJail       
+                            edited method calls and mismatch attribute names                   
    
- * Version 1.03
+        Last Changes Made:  develop()
+                            trade() 
+   
+ * Version 1.04
  */
 
 import java.util.ArrayList;
 
 public class Player extends People{
 
-    ////private ArrayList<Property> properties;
-    //private ArrayList<Card> cards;
+    private ArrayList<Property> properties;
+    private ArrayList<Card> cards;
     private int nPosition;
     private int nDiceRoll;
+    private boolean inJail;
 
     /**
      * This constructor creats a new Player
      */
     public Player(String strName) {
         super(strName, false);
-        ////this.properties = new ArrayList<Property>();
-        //this.cards = new ArrayList<card>();
-        this.nPosition = 0;
+        this.properties = new ArrayList<Property>();
+        this.cards = new ArrayList<Card>();
+        nPosition = 0;
         this.nDiceRoll = 0;
     }
 
@@ -38,46 +38,61 @@ public class Player extends People{
      * @return current position of the player
      */
     public int getPosition() {
-        return this.nPosition;
+        return nPosition;
     }
 
     /**
      * Getter for the cards on hand (One card at a time, first card always)
      * @return first card on hand (if there are any)
      */
-    //public Card getCard() {
-        //if(!cards.isEmpty())
-            //return this.cards.get(0);
-    //}
+    public Card getCard() {
+        if(!cards.isEmpty())
+            return this.cards.get(0);
+        else
+            return null;
+    }
 
     /**
      * Getter for the properties
      * @return properties arrayList
      */
-    //public Property getProperty() {
-        //return this.properties;
-    //}
-    
+    public ArrayList<Property> getProperty() {
+        return this.properties;
+    }    
 
     /**
      * Getter for the last roll value
      * @return last number rolled
      */
-    public Property getLastRoll() {
+    public int getLastRoll() {
         return this.nDiceRoll;
     }
 
     /**
-     * Setter for the Player's positoin
+     * Getter for the is in jail checker
+     * @return true or false if the player is in jail
+     */
+    public boolean getJail() {
+        return this.inJail;
+    }
+
+    /**
+     * Setter for the Player's position
      * @param roll number of spaces the token moves
      */
     public void setPosition(int nRoll) {
-        if(this.nPosition + nRoll > 31) { //checks if the token went through the whole board already
-            this.nPosition = nRoll - (31 - this.nPosition) - 1;
+        if(nPosition + nRoll > 31) { //checks if the token went through the whole board already
+            nPosition = nRoll - (31 - nPosition) - 1;
         }
-        this.nPosition += nRoll;
+        nPosition += nRoll;
     }
-        
+      
+    /**
+     * Setter for the Player's jail status
+     */
+    public void setJail() {
+        this.inJail = !(this.inJail);
+    }
         
     /**
      * Method that performs the rolling of die and the calculation of steps of the Player's token
@@ -85,8 +100,8 @@ public class Player extends People{
      */
     public void roll(GameBoard gameBoard) {
         int nRoll;
-        int start = this.nPosition; //Initializes the starting position of the token
-        //Properties temp;
+        int start = nPosition; //Initializes the starting position of the token
+        Property temp;
         nRoll = (int)((Math.random() * (6 - 1)) + 1 ); //generates a random number form 1 - 6 
         this.nDiceRoll = nRoll;
         
@@ -95,17 +110,17 @@ public class Player extends People{
             if( i > 31 ) { //Checks if the token has reached the end of the board 
                 nRoll = nRoll + start -i;  
                 i = 0;
-                this.nPosition = 0; //this sets the token at start (index 0)
+                nPosition = 0; //this sets the token at start (index 0)
                 start = 0;
-                this.dMoney += 200; //the player received 200$ from the bank
-                gameBoard.getBank().giveMoney(this,200); //the bank shells out 200$
+                dMoney += 200; //the player received 200$ from the bank
+                gameBoard.getBank().giveMoney(200); //the bank shells out 200$
             }
             else {
-                this.nPosition += 1;
+                nPosition += 1;
             }
-            //temp = properties.get(i);
-            //temp.addFootTraffic();
-            //properties.set(i,temp);
+            temp = properties.get(i);
+            temp.addFootTraffic();
+            properties.set(i,temp);
         }
 
     action(gameBoard); //calls the actions to be offered to the player
@@ -116,20 +131,20 @@ public class Player extends People{
      * This lets the player choose from a set of actions he/she may do during the turn
      * @param gameBoard Gameboard where the players are playing on
      */
-    public void action(GameBoard gameboard) {
-        switch(gameBoard.getLand().get(this.nPosition).getLandType()) {
+    public void action(GameBoard gameBoard) {
+        switch(gameBoard.getLand().get(nPosition).getLandType()) {
             case "income":
             case "luxury":
             case "chance":
-            case "corner": gameBoard.getLand().triggerEvent(this, gameboard); break; //if the player lands on a non own-able land tile, call triggerEvent
+            case "corner": gameBoard.getLand().get(nPosition).triggerEvent(this, gameBoard); break; //if the player lands on a non own-able land tile, call triggerEvent
             case "railroad":
             case "utility": 
             case "property": {
-                if(!isMine) { //checks if the current Player owns that piece of land
-                    if (gameBoard.getLand().get(this.nPosition).getOwner() != null) //checks if the landed tile is owned by the current Player
-                        gameBoard.getLand().triggerEvent(this, gameboard);
+                if(!isMine(gameBoard)) { //checks if the current Player owns that piece of land
+                    if (gameBoard.getLand().get(nPosition).getOwner() != null) //checks if the landed tile is owned by the current Player
+                        gameBoard.getLand().get(nPosition).triggerEvent(this, gameBoard);
                     else { //If not, checks if that tile is free to purchase form the bank
-                        if(this.dMoney >= gameBoard.getLand().get(this.nPosition).arrAttributes[0]) { //checks if the current player has sufficient funds before offering to buy that land 
+                        if(dMoney >= gameBoard.getLand().get(nPosition).getDetails()[0]) { //checks if the current player has sufficient funds before offering to buy that land 
                             purchase(gameBoard);
                         }
                     }
@@ -150,21 +165,21 @@ public class Player extends People{
 
     /**
      * Checks if the current Player owns that piece of land
+     * @param gameBoard Gameboard where the players are playing on
      * @return if current Player owns that land
      */
-    public boolean isMine() {
-        return this.properties.contains(gameBoard.getLand().get(this.nPosition));
+    public boolean isMine(GameBoard gameBoard) {
+       return this.properties.contains(gameBoard.getLand().get(nPosition));
     }
-
     /**
      * Purchasing a Land Tile
      * @param gameBoard Gameboard where the players are playing on
      */
     private void purchase (GameBoard gameBoard) {
-        gameBoard.getLand.get(this.nProperty).setOwner(this); //sets the current player as the owner of the land
-        this.properties.add(gameBoard.getLand.get(this.nProperty)); //adds the land to the current Player's list of land owned
-        this.dMoney -= gameBoard.getLand().get(this.nPosition).arrAttributes[0]; //subtracts the money from the current Player
-        gameBoard.getBank().receiveMoney(gameBoard.getLand().get(this.nPosition).arrAttributes[0]); //gives the money to the bank
+        gameBoard.getLand().get(nPosition).setOwner(this); //sets the current player as the owner of the land
+        this.properties.add(gameBoard.getLand().get(nPosition)); //adds the land to the current Player's list of land owned
+        dMoney -= gameBoard.getLand().get(nPosition).getDetails()[0]; //subtracts the money from the current Player
+        gameBoard.getBank().receiveMoney(gameBoard.getLand().get(nPosition).getDetails()[0]); //gives the money to the bank
     } 
 
     /**
@@ -173,19 +188,19 @@ public class Player extends People{
      */
     private void trade(GameBoard gameBoard) {
         //choose which property to trade with (index)
-        int nChosenProperty;
+int nChosenPosition = 0;
         //assuming other end agrees,
-        gameBoard.getLand.get(this.nProperty).setOwner(gameBoard.getLand.get(this.nProperty).getOwner(nChosenProperty));
+        gameBoard.getLand().get(nPosition).setOwner(gameBoard.getLand().get(nChosenPosition).getOwner());
 
-        gameBoard.getLand.get(nChosenProperty).setOwner(this);
+        gameBoard.getLand().get(nChosenPosition).setOwner(this);
     }
 
     /**
      * Develops land of the owner
      */
     private void develop(GameBoard gameBoard) {
-       this.dMoney -=  gameBoard.getLand().get(this.nPosition).arrAttributes[1];
-       gameBoard.getLand().get(this.nPosition).setDevelopment(1);
+       dMoney -=  gameBoard.getLand().get(nPosition).getDetails()[1];
+       gameBoard.getLand().get(nPosition).setDevelopment(1);
     }
 
     /**
@@ -193,8 +208,8 @@ public class Player extends People{
      * @return boolean if the land is eligible
      */
     private boolean eligibleDev (GameBoard gameBoard) {
-        if(this.dMoney >= gameBoard.getLand().get(this.nPosition).arrAttributes[1]) {
-            if( (gameBoard.getLand().get(this.nPosition).getRentCollected() >= gameBoard.getLand().get(this.nPosition).arrAttributes[1]) || (gameBoard.getLand().get(this.nPosition).getFootTraffic() >=  gameBoard.getLand().get(this.nPosition).arrAttributes[8] * gameBoard.getPlayers().length) ) {
+        if(dMoney >= gameBoard.getLand().get(nPosition).getDetails()[1]) {
+            if( (gameBoard.getLand().get(nPosition).getRentCollected() >= gameBoard.getLand().get(nPosition).getDetails()[1]) || (gameBoard.getLand().get(nPosition).getFootTraffic() >=  gameBoard.getLand().get(nPosition).getDetails()[8] * gameBoard.getPlayers()) ) {
                 return true;
             }
             else {
