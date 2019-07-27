a0be79b585e8d75e27f3b0abf8d6b1e2d6833ce9
NeilMatthewLua <NeilMatthewLua>
2019-07-27 21:20:01 +0800

    Version 1.6: EventLog okay

--- src/Controller/StartController.java
+++ src/Controller/StartController.java
@@ -9,8 +9,12 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import static javafx.stage.Modality.APPLICATION_MODAL;
 
 public class StartController {
 
@@ -20,13 +24,68 @@ public class StartController {
         private Label playLabel;
 
 
+//    public void labelClick(MouseEvent e) throws IOException {
+//       if(e.getSource() == playLabel){
+//           Stage stage = (Stage)anchorPane.getScene().getWindow();
+//           FXMLLoader loader = new FXMLLoader();
+//           loader.setLocation(getClass().getResource("/View/ViewInit.fxml"));
+//           Scene scene = new Scene(loader.load());
+//           stage.setScene(scene);
+//       }
+//    }
+
     public void labelClick(MouseEvent e) throws IOException {
-       if(e.getSource() == playLabel){
-           Stage stage = (Stage)anchorPane.getScene().getWindow();
-           FXMLLoader loader = new FXMLLoader();
-           loader.setLocation(getClass().getResource("/View/ViewInit.fxml"));
-           Scene scene = new Scene(loader.load());
-           stage.setScene(scene);
-       }
+        if(e.getSource() == playLabel){
+            Stage stage = (Stage)anchorPane.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/View/EventLog.fxml"));
+
+
+            Scene scene = new Scene(loader.load(),287,410);
+
+            Stage x = new Stage();
+            x.setScene(scene);
+            EventLog eventLog = loader.getController();
+            ArrayList<String> events = new ArrayList<>();
+
+            events.add("Albert paid 10$ to Neil");
+            events.add("Neil rolled a 5!");
+            events.add("Neil bought Abad Santos");
+            events.add("Albert paid 10$ to Neil");
+            events.add("Neil rolled a 5!");
+            events.add("Neil bought Abad Santos");
+            events.add("Albert paid 10$ to Neil");
+            events.add("Neil rolled a 5!");
+            events.add("Neil bought Abad Santos");
+            events.add("Albert paid 10$ to Neil");
+            events.add("Neil rolled a 5!");
+            events.add("Neil bought Abad Santos");
+            events.add("Albert paid 10$ to Neil");
+            events.add("Neil rolled a 5!");
+            events.add("Neil bought Abad Santos");events.add("Albert paid 10$ to Neil");
+            events.add("Neil rolled a 5!");
+            events.add("Neil bought Abad Santos");
+            events.add("Albert paid 10$ to Neil");
+            events.add("Neil rolled a 5!");
+            events.add("Neil bought Abad Santos");
+            events.add("Albert paid 10$ to Neil");
+            events.add("Neil rolled a 5!");
+            events.add("Neil bought Abad Santos");
+            events.add("Albert paid 10$ to Neil");
+            events.add("Neil rolled a 5!");
+            events.add("Neil bought Abad Santos");events.add("Albert paid 10$ to Neil");
+            events.add("Neil rolled a 5!");
+            events.add("Neil bought Abad Santos");
+
+
+
+
+            eventLog.initialize(events);
+            x.initModality(APPLICATION_MODAL);
+            x.setResizable(false);
+            x.setX(900);
+            x.setY(30);
+            x.show();
+        }
     }
 }
